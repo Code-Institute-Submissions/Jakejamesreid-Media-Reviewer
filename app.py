0017c186383018aa15ecc4dd03c5fc4b8fc6f1bb
jakejamesreid <jakejamesreid@gmail.com>
2020-08-02 17:17:57 +0100

    Added venv and have initial basic website in operation

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
 app.secret_key = "Secretproductionkey1234"
+app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"), 
-    port=os.environ.get("PORT"),
-    debug=True)
+    app.run(host=os.environ.get("IP"), port=os.environ.get("PORT"), debug=True)
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Media Reviewer
+
+This is a website for users to post reviews on their favourite media such as video games, movies and TV Shows
